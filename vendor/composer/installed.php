9c31169136111492923f9270097c486b6eb7a7a1
kennedy-rain <116776020+kennedy-rain@users.noreply.github.com>
2023-08-07 12:34:32 -0500

    update padding

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'isueit/iastate_theme',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '2f51dc5ece3b002ac755967d0329fde0cf4380d8',
+        'type' => 'drupal-custom-theme',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'isueit/iastate_theme' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '2f51dc5ece3b002ac755967d0329fde0cf4380d8',
+            'type' => 'drupal-custom-theme',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
